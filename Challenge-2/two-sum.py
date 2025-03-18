322626d8224284d758f25626baa83af894a04863
joseph-mv <joseph.mv@wecodecommunity.com>
2025-03-18 14:05:42 +0530

    Add solution for two sum

---
+++ Challenge-2/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Given an array of integers nums and an integer target, return indices 
+        of the two numbers such that they add up to target.
+        """
+        hash={}
+        for idx in range(0 ,len(nums)):
+            complement=target-nums[idx]
+            if complement in hash:
+                return [idx,hash[complement]]
+            hash[nums[idx]]=idx
