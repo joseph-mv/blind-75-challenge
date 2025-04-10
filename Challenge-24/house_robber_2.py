b136db0d6e3201d3aa2dbbf01b26465e240aabc0
joseph-mv <josephmv43@gmail.com>
2025-04-10 15:48:47 +0530

    fix:add two edge conditions in house_robber_2

--- Challenge-24/house_robber_2.py
+++ Challenge-24/house_robber_2.py
@@ -10,10 +10,13 @@ class Solution:
         The maximum amount of money the robber can steal.
     """
     def rob(self, nums: list[int]) -> int:
+        n=len(nums)
+        if n==0:
+            return 0
+        if n==1:
+            return nums[0]
         def rob_linear( nums: list[int]) -> int:
             n=len(nums)
-            if n==0:
-                return 0
             if n==1:
                 return nums[0]
 
