c685d65d4bbc7c4b6ee780b5ead8a80c6a0f0841
joseph-mv <josephmv43@gmail.com>
2025-05-26 22:10:16 +0530

    add solution for lowest comman ancestor of a binary search tree

---
+++ Challenge-71/low_cmn_anctr_of_BST.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        """
+        Find out Lowest Common Ancestor of a Binary Search Tree
+
+        Args:
+            root : The root of the binary search tree.
+            p : First node.
+            q : Second node.
+
+        Returns:
+            TreeNode of lowest common ancestor of p and q.
+
+        """
+
+        while True:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
